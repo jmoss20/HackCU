b8d81b4d5f7cba3c58f2d9e10d397cd490e88d98
Sam Pochyly <sampocs@resgw2-193-156-dhcp.int.colorado.edu>
2017-04-23 10:50:43 -0600

    webpage

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 var app = require('express')();
 var server = require('http').Server(app).listen(3000);
 var io = require('socket.io')(server);
-console.log('server started')
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/hackcuwebpage.html');
+});
+
+app.get('/emoji', function (req, res) {
+  res.sendFile(__dirname + '/popup.html');
 });
 
 io.on('connection', function (socket) {
-	console.log("A user is connected")
-	socket.on('message', function(data) {
-		console.log('Received: ' + data);
-		socket.emit('sendres', data);
-	});
-	socket.on('disconnect', function () {
-    	console.log("A user is disconnected");
-	});
+	socket.emit('emojis',obj);
+	socket.emit('emojis',obj2);
+	socket.emit('emojis',obj3);
+	socket.emit('emojis',obj4);
 });
+
+obj = {
+	user_id: 'sam',
+	emoji: "1",
+	confidence: ""
+}
+obj2 = {
+	user_id: 'connor',
+	emoji: "2",
+	confidence: ""
+}
+obj3 = {
+	user_id: 'barrett',
+	emoji: "3",
+	confidence: ""
+}
+obj4 = {
+	user_id: 'connor',
+	emoji: "4",
+	confidence: ""
+}
